69c0566788416e06128376ab8a33ab3f8d3b6a77
dictions <iwillinc@gmail.com>
2013-08-23 14:16:37 -0400

    Install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,11 @@
 # you need the code CLI tools YOU FOOL.
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
+# install osx defaults
 bash ~/.dotfiles/.osx
+
+# install brew packages
 bash ~/.dotfiles/.brew
+
+# global gitignore
+git config --global core.excludesfile ~/.global_ignore
